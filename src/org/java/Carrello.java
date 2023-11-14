2b2bbe1377c3051c9f5d8ea52c50584976c824a9
MichaelMantovani <michmantovani@gmail.com>
2023-11-14 15:37:47 +0100

    add validation on inputProdotto

--- src/org/java/Carrello.java
+++ src/org/java/Carrello.java
@@ -17,13 +17,50 @@ public static void main(String[] args) {
 			nuovoProdotto=false; 
 			break; 
 		}else if (!risposta.equals("si")) {
-			System.out.println("Rispondimi soltanto si o no");
+			System.out.println("**Rispondimi soltanto si o no**");
 			continue;
 		}
 		
 		System.out.println("Quale tipo di prodotto vuoi inserire? (Smartphone, Televisore , Cuffie)");
-		String inputProdotto = in.nextLine();
-		System.out.println(inputProdotto);
+		String inputProdotto = in.nextLine().toLowerCase();
+		
+	
+			if (!inputProdotto.equals("smartphone") || !inputProdotto.equals("televisore") || !inputProdotto.equals("cuffie")) {
+				System.out.println("**Rispondimi soltanto Smartphone, Televisore o Cuffie**");
+				continue;
+			}
+		
+		
+		String nome; 
+		String descrizione; 
+		float prezzoBase;
+		float IVA;
+		String codiceIMEI;
+		int memoria;
+		int dimensioni;
+		boolean smartTV;
+		String colore;
+		boolean wireless;
+		
+		System.out.println("come si chiama il prodotto ?");
+		nome = in.nextLine();
+		System.out.println("Scrivi una piccola descrizione ?");
+		descrizione = in.nextLine();
+		System.out.println("Qual è il prezzo?");
+		prezzoBase = in.nextFloat();
+		System.out.println("Quanto è l'IVA  da applicare su questo prodotto?");
+		IVA = in.nextFloat();
+		
+		if(inputProdotto.equals("smartphone")) {
+			System.out.println("iphone");
+		} else if (inputProdotto.equals("televisore")) {
+			System.out.println("TV");
+		} else  {
+			System.out.println("Bose");
+		} 
+		
+		
+		
 	}
 	
 	
